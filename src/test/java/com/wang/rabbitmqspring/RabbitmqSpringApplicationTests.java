c4ce84f99eb3f95a3dd0101ad49a56b19cdc624b
WinKnight <wangchao2@iiotos.com>
2020-01-04 17:33:30 +0800

    Initial commit

---
+++ src/test/java/com/wang/rabbitmqspring/RabbitmqSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.rabbitmqspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
